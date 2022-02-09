e2fd2ef7a00f2bb5187a8933fb231996f23b0d6a
Scurrra <iscurrra@gmail.com>
2022-02-09 19:03:42 +0300

    Initial commit

---
+++ lab1/lab1.jl
@@ -0,0 +1,14 @@
+### A Pluto.jl notebook ###
+# v0.18.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ c5717b80-88bf-11ec-18bd-258b0006abe5
+begin
+	import Pkg
+	Pkg.activate()
+end
+
+# ╔═╡ Cell order:
+# ╠═c5717b80-88bf-11ec-18bd-258b0006abe5
